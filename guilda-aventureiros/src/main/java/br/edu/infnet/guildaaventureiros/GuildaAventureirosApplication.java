9c6699c3dc4654dcbcb8cc565e0f3c82512af75b
isaac <isaaccrvg98@hotmail.com>
2026-04-05 21:22:28 -0300

    initial add

---
+++ guilda-aventureiros/src/main/java/br/edu/infnet/guildaaventureiros/GuildaAventureirosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.guildaaventureiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildaAventureirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuildaAventureirosApplication.class, args);
+	}
+
+}
